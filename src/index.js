ff86bb29bc2b4053be697bbe457fea1964552ec6
Harry-Yeh <Harryyeh428@gmail.com>
2026-07-21 16:35:19 +0800

    chore(init): bootstrap TypeScript project structure

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+async function main() {
+    console.log("Harness-Lab Ready");
+}
+main();
+//# sourceMappingURL=index.js.map
